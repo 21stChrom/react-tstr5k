1ee6fdc0f90c53c46fd6411af7420f7e391bf892
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-04-09 21:49:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+const renderFiles = () => {
+
+  const containers = []; for (let i = 0; i < 30; i++) {
+
+  const file = files[i]; containers.push(
+
+  <div
+
+  key={i}
+
+  className={file-container
+
+  hidden-${i + 1}'}
+
+  onClick={() => console.log('File ${i + 1}
+
+  clicked')}
+  >
+
+  {file? <span>{file.name}
+
+  <\span> : null}
+  </div>
+  );
+  }
+
+  return (
+
+  <div
+
+  className="outer-container" onMouseMove={handleMouseEnter} onMouseLe ave={handleMouseLeave}
+  > {containers} </div>
+
+  );
+
+  };
+}
